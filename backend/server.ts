5204a056bd2ee0b4481cd5017380a4b2fb615b8a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-11-27 22:29:55 +0900

    feat: Express と Socket.IO を使ったバックエンドサーバーのセットアップ

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,24 @@
 import express from "express";
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+import cors from "cors";
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg: string) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconect", () => {
+    console.log("user disconnected");
+  });
 });
-app.listen(3000);
+app.listen(5000);
