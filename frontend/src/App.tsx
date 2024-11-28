a8046e2f4da030ae417338d56477650523aa5429
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-11-28 19:11:12 +0900

    feat: 初回にサインアップを要求

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,66 +1,31 @@
-import React, { useState, useEffect, useRef } from "react";
-import { io, Socket } from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./config/firebase";
+import Chat from "./components/Chat";
+import SignUp from "./components/SignUp";
 
-const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL;
-
-interface Message {
-  username: string;
-  message: string;
-  timestamp: Date;
-}
-
-export default function App() {
-  const [message, setMessage] = useState<string>("");
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [username, setUsername] = useState<string>(
-    "User" + Math.floor(Math.random() * 1000),
-  );
-
-  const socketRef = useRef<Socket | null>(null);
+const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    socketRef.current = io(SOCKET_SERVER_URL);
-
-    socketRef.current.on("load message", (msgs: Message[]) => {
-      setMessages(msgs);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
     });
 
-    socketRef.current.on("chat message", (msg: Message) => {
-      setMessages((prev) => [...prev, msg]);
-    });
-    return () => {
-      socketRef.current?.disconnect();
-    };
+    return () => unsubscribe();
   }, []);
-
-  const sendMessage = () => {
-    if (message.trim()) {
-      const msg: Message = { username, message, timestamp: new Date() };
-      socketRef.current?.emit("chat message", msg);
-      setMessage("");
-    }
-  };
 
   return (
     <>
-      <h1>chat app</h1>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="type message"
-      />
-      <button onClick={sendMessage}>submit</button>
-      <h2>chat history</h2>
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>
-            <span>
-              {msg.username}: {msg.message}
-            </span>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>{user !== null ? <Chat /> : <SignUp />} </>
+      )}
     </>
   );
-}
+};
+
+export default App;
--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,46 +1,50 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import { auth, db} from "../config/firebase";
+import { auth, db } from "../config/firebase";
 import React, { useState } from "react";
 
 export default function SignUp() {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [username, setUsername] = useState<string>("");
-    
-    const handleSignup = async (e: React.FormEvent) => {
-        e.preventDefault();
-         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-         const user = userCredential.user;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
 
-         await setDoc(doc(db, "users", user.uid), {
-            username: username,
-            email: user.email,
-            createdAt: new Date()
-         });
-    }
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    const user = userCredential.user;
 
-    return (
-        <form onSubmit={(e) => handleSignup(e)}>
-        <input
-          type="text"
-          placeholder="ユーザー名"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="メールアドレス"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="パスワード"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">サインアップ</button>
-      </form>
-    )
+    await setDoc(doc(db, "users", user.uid), {
+      username: username,
+      email: user.email,
+      createdAt: new Date(),
+    });
+  };
+
+  return (
+    <form onSubmit={(e) => handleSignup(e)}>
+      <input
+        type="text"
+        placeholder="ユーザー名"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="メールアドレス"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="パスワード"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">サインアップ</button>
+    </form>
+  );
 }
